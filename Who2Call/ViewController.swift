52ae2c2c1d648005969846e5e6142d81b3dbdbdb
Tomasz Kielar <tomek.kielar00@gmail.com>
2019-10-19 21:56:53 +0200

    Initial Commit

---
+++ Who2Call/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Who2Call
+//
+//  Created by Tomasz Kielar on 19/10/2019.
+//  Copyright © 2019 Definitely Not Devs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
